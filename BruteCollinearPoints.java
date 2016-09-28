0c12b5c41c5f3fc485aa38de333264aa06c13c42
Михаил Зулкарнеев <zulkarneev@mail.ru>
2016-09-28 15:19:36 +0300

    First commit

---
+++ BruteCollinearPoints.java
@@ -0,0 +1,38 @@
+/**
+ * @author 			mikhail
+ * Created			28.09.2016
+ * Last modified  	28.09.2016
+ * 
+ */
+
+public class BruteCollinearPoints
+{
+	
+	/**
+	 * Finds all line segments containing 4 points
+	 * @param points
+	 */
+	public BruteCollinearPoints(Point[] points)
+	{
+		
+		
+	}
+	
+	/**
+	 * The number of line segments
+	 * @return
+	 */
+	public int numberOfSegments()
+	{
+		return 0;
+	}
+	
+	/**
+	 * The line segments
+	 * @return
+	 */
+	public LineSegment[] segments()
+	{
+		return null;
+	}
+}
